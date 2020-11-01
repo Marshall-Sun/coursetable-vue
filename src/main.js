e7807ac556e402ff1980f26b908ac88ef9fd4387
Marshall-Sun <910204019@qq.com>
2020-11-01 23:10:13 +0800

    add modularized

--- src/main.js
+++ src/main.js
@@ -1,23 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-import VueResource from 'vue-resource'
+import VueResource from "vue-resource";
 
-import {Layout} from 'ant-design-vue';
-import {Cascader} from 'ant-design-vue';
-import {Table} from 'ant-design-vue';
+import { Layout } from "ant-design-vue";
+import { Cascader } from "ant-design-vue";
+import { Table } from "ant-design-vue";
 
-import 'ant-design-vue/lib/layout/style/css'
-import 'ant-design-vue/lib/cascader/style/css'
-import 'ant-design-vue/lib/table/style/css'
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
-
-Vue.use(VueResource)
-Vue.use(Layout)
-Vue.use(Cascader)
-Vue.use(Table)
+Vue.use(VueResource);
+Vue.use(Layout);
+Vue.use(Cascader);
+Vue.use(Table);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   publicPath: "/coursetable-vue/",
   outputDir: "docs",
+  chainWebpack: config => {
+    config
+    .plugin('webpack-bundle-analyzer')
+    .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+  },
 };
